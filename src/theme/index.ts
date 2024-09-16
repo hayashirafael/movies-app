9cedae738650bd117ed32b4b696289e2db09858d
Rafael Hayashi <rafaelhayashi8@gmail.com>
2024-09-16 17:26:25 -0300

    chore: add theme config

---
+++ src/theme/index.ts
@@ -0,0 +1,10 @@
+export default {
+  COLORS: {
+    ORANGE: '#EC8B00',
+    WHITE: '#FFFFFF',
+    GREY200: '#A9A9A9',
+    GREY500: '#2E2F33',
+    GREY700: '#16171B',
+    RED: '#EB5858FF'
+  }
+};
