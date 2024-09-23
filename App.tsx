3b2f25a5ab8d19dc3c6c6f778872b99587c048e2
Rafael Hayashi <rafaelhayashi8@gmail.com>
2024-09-23 18:00:30 -0300

    chore: add movie provider & nunito google font

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { SignIn } from '@screens/SignIn';
+import '@libs/dayjs';
 import theme from './src/theme';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components/native';
 import { AuthContextProvider } from '@contexts/AuthContext';
 import { Routes } from '@routes/index';
+import { Nunito_400Regular, Nunito_700Bold, useFonts } from '@expo-google-fonts/nunito';
+import { Loading } from '@components/Loading';
+import { MovieContextProvider } from '@contexts/MovieContext';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold });
+  if (!fontsLoaded) return <Loading />
+
   return (
     <SafeAreaProvider style={{ flex: 1 }} >
       <ThemeProvider theme={theme}>
         <StatusBar
-          backgroundColor="transparent"
+          backgroundColor={theme.COLORS.GREY700}
         />
         <AuthContextProvider>
-          <Routes />
+          <MovieContextProvider>
+            <Routes />
+          </MovieContextProvider>
         </AuthContextProvider>
       </ThemeProvider>
     </SafeAreaProvider>
