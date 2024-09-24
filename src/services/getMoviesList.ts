79842b4377664a1b56526a8463a3f652ef695baf
Rafael Hayashi <rafaelhayashi8@gmail.com>
2024-09-24 19:59:05 -0300

    feat: add get movie list service

--- src/services/getMoviesList.ts
+++ src/services/getMoviesList.ts
@@ -1,10 +1,10 @@
-import { MovieDTO } from "@dtos/movie";
+import { MoviesDTO } from "@dtos/movie";
 import { api } from "./api";
 
 export async function getMoviesList() {
   try {
-    const {data} = await api.get<MovieDTO>('/movie/popular');
-    return data;
+    const {data} = await api.get<MoviesDTO>('/movie/popular');
+    return data.results;
   } catch (error) {
     console.log(error);
     return [];
