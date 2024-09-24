fd9dc266d963d40b30df70b80e8584844f21e410
Rafael Hayashi <rafaelhayashi8@gmail.com>
2024-09-24 19:36:40 -0300

    feat: add netinfo & nunito fonts

--- App.tsx
+++ App.tsx
@@ -5,12 +5,15 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components/native';
 import { AuthContextProvider } from '@contexts/AuthContext';
 import { Routes } from '@routes/index';
-import { Nunito_400Regular, Nunito_700Bold, useFonts } from '@expo-google-fonts/nunito';
+import { Nunito_400Regular, Nunito_500Medium, Nunito_700Bold, useFonts } from '@expo-google-fonts/nunito';
 import { Loading } from '@components/Loading';
 import { MovieContextProvider } from '@contexts/MovieContext';
+import { NetInfo } from '@components/NetInfo';
+import { useNetInfo } from "@react-native-community/netinfo";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold });
+  const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_500Medium, Nunito_700Bold });
+  const { isConnected } = useNetInfo();
   if (!fontsLoaded) return <Loading />
 
   return (
@@ -19,6 +22,9 @@ export default function App() {
         <StatusBar
           backgroundColor={theme.COLORS.GREY700}
         />
+        {
+          !isConnected && <NetInfo />
+        }
         <AuthContextProvider>
           <MovieContextProvider>
             <Routes />
