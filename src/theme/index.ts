92d028d45ecb13ecc63e17336efff2271da1685a
Rafael Hayashi <rafaelhayashi8@gmail.com>
2024-09-24 19:58:20 -0300

    style: add font family at theme file

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -6,5 +6,10 @@ export default {
     GREY500: '#2E2F33',
     GREY700: '#16171B',
     RED: '#EB5858FF'
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Nunito_400Regular',
+    MEDIUM: 'Nunito_500Medium',
+    BOLD: 'Nunito_700Bold'
   }
 };
